57e14768f28e12c61678ccfe41f466072a44c60e
Zhiqi <shzhiqi@outlook.com>
2017-10-09 15:20:41 +0800

    fix the problem when xmin == 0 or ymin == 0
    when xmin == 0 or ymin == 0 will get the error bounding box.

--- lib/datasets/pascal_voc.py
+++ lib/datasets/pascal_voc.py
@@ -206,7 +206,9 @@ class pascal_voc(imdb):
             bbox = obj.find('bndbox')
             # Make pixel indexes 0-based
             x1 = float(bbox.find('xmin').text) - 1
+            if x1 < 0: x1 = 0.;
             y1 = float(bbox.find('ymin').text) - 1
+            if y1 < 0: y1 = 0.;
             x2 = float(bbox.find('xmax').text) - 1
             y2 = float(bbox.find('ymax').text) - 1
             cls = self._class_to_ind[obj.find('name').text.lower().strip()]
